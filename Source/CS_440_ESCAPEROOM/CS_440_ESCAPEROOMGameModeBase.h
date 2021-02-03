9834f39fd5c82720f67dac1dcc559ba37d45859e
Jay Thomas Blankenship <Jayblankenship@outlook.com>
2021-02-03 10:20:58 -0600

    Revert "third try for commiting unreal template project"
    This reverts commit fdc1d1305739699bdeb96aae965a9398e2279df8.

---
+++ Source/CS_440_ESCAPEROOM/CS_440_ESCAPEROOMGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CS_440_ESCAPEROOMGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CS_440_ESCAPEROOM_API ACS_440_ESCAPEROOMGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
