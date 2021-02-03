fdc1d1305739699bdeb96aae965a9398e2279df8
Jay Thomas Blankenship <Jayblankenship@outlook.com>
2021-02-03 10:20:31 -0600

    third try for commiting unreal template project
    third try hopefully it works

--- Source/CS_440_ESCAPEROOM/CS_440_ESCAPEROOM.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CS_440_ESCAPEROOM.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CS_440_ESCAPEROOM, "CS_440_ESCAPEROOM" );
--- Source/CS_440_ESCAPEROOM/CS_440_ESCAPEROOMGameModeBase.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "CS_440_ESCAPEROOMGameModeBase.h"
-
