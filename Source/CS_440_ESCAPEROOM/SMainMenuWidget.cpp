74ebf0f9798f762b61c14f0addc72a407cd02fee
damezq2 <59977665+damezq2@users.noreply.github.com>
2021-02-07 19:23:02 -0600

    Added buttons plus some styling elements.

--- Source/CS_440_ESCAPEROOM/SMainMenuWidget.cpp
+++ Source/CS_440_ESCAPEROOM/SMainMenuWidget.cpp
@@ -11,25 +11,111 @@ BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
 void SMainMenuWidget::Construct(const FArguments& InArgs)
 {
 	OwnerHUD = InArgs._OwnerHUD;
+	
+	bCanSupportFocus = true;
+
+	const FMargin padding = FMargin(200.f, 100.f);
+
+	const FText gameTitle = LOCTEXT("gameTitle", "EscapeRoom");
+	const FText playText = LOCTEXT("playBtn", "Play");
+	const FText settingText = LOCTEXT("settingsBtn", "Settings");
+	const FText quitText = LOCTEXT("quitBtn", "Quit");
+
+	FSlateFontInfo titleFont = FCoreStyle::Get().GetFontStyle("Arial");
+	titleFont.Size = 60.f;
+
+	FSlateFontInfo btnFont = FCoreStyle::Get().GetFontStyle("Arial");
+	btnFont.Size = 30.f;
 
 	
-	ChildSlot.VAlign(VAlign_Fill).HAlign(HAlign_Fill)
-		[
+	ChildSlot
+	[
 		SNew(SOverlay)
 		+ SOverlay::Slot()
+		.VAlign(VAlign_Fill)
+		.HAlign(HAlign_Fill)
 		[
 			SNew(SImage)
-			.ColorAndOpacity(FColor::Black)
+			.ColorAndOpacity(FColor::Silver)
 		]
 		+ SOverlay::Slot()
-		.HAlign(HAlign_Center)
-		.VAlign(VAlign_Top)
+		.VAlign(VAlign_Fill)
+		.HAlign(HAlign_Fill)
+		.Padding(padding)
 		[
-			SNew(STextBlock)
-			.Text(LOCTEXT("Title", "EscapeRoom"))
+			SNew(SVerticalBox)
+			+ SVerticalBox::Slot()
+			[
+				SNew(STextBlock)
+				.Text(gameTitle)
+				.Font(titleFont)
+				.ColorAndOpacity(FColor::Red)
+				.Justification(ETextJustify::Center)
+			]
+			
+			//play button
+			+ SVerticalBox::Slot()
+			.Padding(FMargin(10.f))
+			[
+				SNew(SButton)
+				.OnClicked(this, &SMainMenuWidget::OnPlayClicked)
+				[
+					SNew(STextBlock)
+					.Font(btnFont)
+					.Text(playText)
+					.Justification(ETextJustify::Center)
+				]
+			]
+
+			//settings button
+			+ SVerticalBox::Slot()
+			.Padding(FMargin(10.f))
+			[
+				SNew(SButton)
+				.OnClicked(this, &SMainMenuWidget::OnSettingsClicked)
+				[
+					SNew(STextBlock)
+					.Font(btnFont)
+					.Text(settingText)
+					.Justification(ETextJustify::Center)
+				]
+			]
+
+			//quit button
+			+ SVerticalBox::Slot()
+			.Padding(FMargin(10.f))
+			[
+				SNew(SButton)
+				.OnClicked(this, &SMainMenuWidget::OnQuitClicked)
+				[
+					SNew(STextBlock)
+					.Font(btnFont)
+					.Text(quitText)
+					.Justification(ETextJustify::Center)
+					
+				]
+			]
+
 		]
 	];
-	
+}
+
+//TODO: Set play functionality here
+FReply SMainMenuWidget::OnPlayClicked() const
+{
+
+}
+
+//TODO: Set settings functionality here
+FReply SMainMenuWidget::OnSettingsClicked() const
+{
+
+}
+
+//TODO: Set quit functionality here
+FReply SMainMenuWidget::OnQuitClicked() const
+{
+
 }
 
 #undef LOCTEXT_NAMESPACE
